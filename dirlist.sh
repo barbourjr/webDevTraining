6564951f6f7ac354616bf3c58c4f3633168de5e3
James Barbour, Jr <james.barbour@caci.com>
2023-01-25 16:17:41 -0500

    Dir List var change

--- dirlist.sh
+++ dirlist.sh
@@ -2,16 +2,16 @@
 
 #find . -mindepth 1 -maxdepth 2 -type d > /home/barbourj/list_of_dirs.txt
 > /home/barbourj/list_of_dirs.txt
-for d in */ ; do
-    [ -L "${d%/}" ] && continue
-    if [ -d "${d/}" ] ; then
-        echo "$d"
-        echo "$d" >> /home/barbourj/list_of_dirs.txt
-        cd ${d}
-        for e in */ ; do
-            if [ -d "$e" ] ; then
-                echo "$d$e"
-                echo "$d$e" >> /home/barbourj/list_of_dirs.txt
+for r in */ ; do
+    [ -L "${r%/}" ] && continue
+    if [ -r "${r/}" ] ; then
+        echo "$r"
+        echo "$r" >> /home/barbourj/list_of_dirs.txt
+        cd ${r}
+        for s in */ ; do
+            if [ -r "$s" ] ; then
+                echo "$r$s"
+                echo "$r$s" >> /home/barbourj/list_of_dirs.txt
             fi
             done
         cd ../
