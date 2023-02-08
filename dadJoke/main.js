5b728eb55e93cf9ca1b028fa659ab58aa3bcfcce
James Barbour, Jr <james.barbour@caci.com>
2023-02-08 17:22:51 -0500

    WIP Added Error handling but still getting syntax error on API call

--- dadJoke/main.js
+++ dadJoke/main.js
@@ -1,12 +1,30 @@
 const mybutton = document.getElementById('dadJokeButton');
 const myTextArea = document.getElementById('jokeArea');
+const url = 'https://icanhazdadjoke.com/'
+async function getJoke(url = '', data = {}) {
+  //const userName = 'barbourjr'
+  //const repo = 'webDevTraining'
+  let myJSON;
+  const myHeaders = new Headers();
+  myHeaders.append('Accept', 'application/json');
 
-async function getJoke() {
-    //const userName = 'barbourjr'
-    //const repo = 'webDevTraining'
-    let response = await fetch('curl -H "Accept: application/json" https://icanhazdadjoke.com/');
-    let myJSON = await response.json();
-    return JSON.parse(myJSON);
+  try {
+    const response = await fetch(url, {
+      method: 'GET', // *GET, POST, PUT, DELETE, etc.
+      headers: myHeaders,
+      mode: 'cors',
+      cache: 'default',
+    })
+    myJSON = await response.json();
+  } catch (error) {
+    if (error instanceof SyntaxError) {
+      // Unexpected token < in JSON
+      console.log('There was a SyntaxError', error);
+    } else {
+      console.log('There was an error', error);
+    }
+  } if (myJSON) {
+    console.log('Use the JSON here!', JSON.stringify(myJSON));
   }
-getJoke().then(myJSON => console.log(data));
+}
 
